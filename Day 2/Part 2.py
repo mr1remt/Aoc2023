e359ce216a5c6d02118a2d8b2a3771cbab5e5159
Jonatan <jonatan1124@hotmail.com>
2023-12-09 15:21:07 +0100

    Day 2

--- Day 2/Part 2.py
+++ Day 2/Part 2.py
@@ -1,31 +1,46 @@
 possibleGames = 0
+sum = 0
+
+times = 0
+
+def isItBigger(currNums, maxNums):
+	for i in [0,1,2]:
+		if currNums[i] > maxNums[i]: maxNums[i] = currNums[i]
+	return currNums, maxNums
+
 input = open('Day 2\input.txt')
 for line in input:
+	times += 1 
 	for char in [",",";",":"]:
 		line = line.replace(char, " " + char)
 	lineList = line.split()
 	lineList.pop(0) 
 	rgbList = [0,0,0]
+	highestNums = [0,0,0]
 	prevNum = 0
 	possible = True
 	for item in lineList:
 		if item.isdigit():
-			if item == lineList[0]:
-				continue
 			prevNum = int(item)
 		else: 
-			if item == "red":
+			if item == "red": 
 				rgbList[0] = rgbList[0] + prevNum
+				rgbList, highestNums = isItBigger(rgbList, highestNums)
 			if item == "green":
 				rgbList[1] = rgbList[1] + prevNum
+				rgbList, highestNums = isItBigger(rgbList, highestNums)
 			if item == "blue":
 				rgbList[2] = rgbList[2] + prevNum
+				rgbList, highestNums = isItBigger(rgbList, highestNums)
 			if item == ";":
 				if rgbList[0] <= 12 and rgbList[1] <= 13 and rgbList[2] <= 14 and possible == True:
 					rgbList = [0,0,0]
 					possible = True
-				else:
+				else: 
+					rgbList = [0,0,0]
 					possible = False
 	if rgbList[0] <= 12 and rgbList[1] <= 13 and rgbList[2] <= 14 and possible == True:
 		possibleGames += int(lineList[0])
-print(possibleGames)
+	sum += highestNums[0] * highestNums[1] * highestNums[2]
+
+print(possibleGames, sum)
